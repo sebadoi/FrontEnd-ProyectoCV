84c93f0ece01d9ed77e5a1ba71c45da4bb21254a
Sebastian Doi <doisebastian@gmail.com>
2023-01-29 20:23:53 -0300

    Versión final del CV

---
+++ JavaCV.js
@@ -0,0 +1,11 @@
+// Generar información complementaria//
+
+function mostrar1() {
+    document.getElementById("informacion1").style.display = 'block';
+    
+}
+function mostrar2() {
+document.getElementById("informacion2").style.display = 'block'
+}
+
+
